07e707128e988f8c28329b6d2bc126ae5681edaa
sabukittu <sabukittu@gmail.com>
2019-04-17 17:42:46 +0530

    Modified with some changes

--- gold.py
+++ gold.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+## This script is used to get data from goldpricez.com using an API 
+## request and store the data to database 'gold_rate' 
+## Then fetch the data from database and will send a mail with 
+## 'GramPrice', 'PavanPrice', 'Status'.
+
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -9,84 +14,109 @@ import requests
 import json
 
 
+## get_api_req() function is used to get api request from goldpricez.com
+## its in JSON format
 def get_api_req():
-   URL = 'http://goldpricez.com/api/rates/currency/inr/measure/gram'
-   api_key = {'X-API-KEY': '9ee965c9c260543dfdd055e3718789019ee965c9'}
-   x = requests.get(url = URL, params = api_key)
-   y = x.json()
-   data = json.loads(y)
-   return data
 
-data = get_api_req()
+	URL = 'http://goldpricez.com/api/rates/currency/inr/measure/gram'
+	api_key = {'X-API-KEY': '9ee965c9c260543dfdd055e3718789019ee965c9'}
+
+	x = requests.get(url = URL, params = api_key)
+	y = x.json()
+	data = json.loads(y)
+
+	return data
+
+
+## These 2 line of code is using to establish a connection 
+## with the database.
 mydb = mysql.connector.connect(host='localhost', database='gold_rate', user='root', password='redhat')
 cursor = mydb.cursor()
 
 
+## db_query() function is used to query the details from database gold_rate
+## the return 'd' value will be dict contain list
 def db_query(cursor):
-   ii = ("SELECT GraminINR FROM RateINR ORDER BY CurrentDate DESC LIMIT 0,2")
-   cursor.execute(ii)
-   fetch = cursor.fetchall()
-   prev = (list(fetch[1]))
-   last = (list(fetch[0]))
-   d = (float(last[0]))
-   e = (float(prev[0]))
-   previous_rate = (8 * e)
-   latest_rate = (8 * d)
-   diff = (latest_rate - previous_rate)
-   if diff > 0:
-     stats = 'HIGH'
-   else:
-     stats = 'LOW'
-   calc = [latest_rate, previous_rate, diff, stats]
 
-   oo = ("SELECT * FROM RateINR ORDER BY CurrentDate DESC LIMIT 0,2")
-   cursor.execute(oo)
-   fetch = cursor.fetchall()
-   d = {}
-   l = (list(fetch[0]))
-   p = (list(fetch[1]))
-   d['latest'] = l
-   d['prev'] = p
-   d['calc'] = calc
-   return d
+	ii = ("SELECT * FROM RateINR ORDER BY CurrentDate DESC LIMIT 0,2")
+	cursor.execute(ii)
+	fetch = cursor.fetchall()
 
+	prev = (list(fetch[1]))
+	pr = (prev[1])
+	last = (list(fetch[0]))
+	la = (last[1])
+
+	f = (float(la))
+	e = (float(pr))
+
+	previous_rate = (8 * e)
+	latest_rate = (8 * f)
+	diff = (latest_rate - previous_rate)
+	
+	if diff > 0:
+		stats = 'HIGH'
+	else:
+		stats = 'LOW'
+	
+	calc = [latest_rate, previous_rate, diff, stats]
+	d = {}
+	d['latest'] = last
+	d['prev'] = prev
+	d['calc'] = calc
+
+	return d
+
+
+## These are the values of rates fetched from
+## db_query() function 
+data = get_api_req()
 a = db_query(cursor)
+
 cu_dt = (a['latest'][3])
 up_dt = (a['latest'][2])
 gm_lr = (round((float(a['latest'][1])),2))
 gm_pr = (round((float(a['prev'][1])),2))
 pv_lr = (round((float(a['calc'][0])),2))
 pv_pr = (round((float(a['calc'][1])),2))
+
 diff = (round((float(a['calc'][2])),2))
 stat = (a['calc'][3])
 
 
-def db_insert(mydb, cursor, lis):
-	add = ("INSERT INTO RateINR "
-         		"(USDtoINR, GraminINR, UpdateTime, CurrentDate, CurrentTime) "
-             	"VALUES (%s, %s, %s, CURDATE(), CURTIME())")
-	cursor.execute(add, lis)
-	mydb.commit()
-
-
+## add_data() function is using to insert the data to database gold_rate
+## the values are fetched from api_get_req()
 def add_data(mydb, cursor, data):
+
 	z = []
 	z.append(data["usd_to_inr"])
 	z.append(data["gram_in_inr"])
 	z.append(data["gmt_inr_updated"])
 	lis = (tuple(z))
+
 	try:
-	    resp = db_insert(mydb, cursor, lis)
+		add = ("INSERT INTO RateINR "
+				"(USDtoINR, GraminINR, UpdateTime, CurrentDate, CurrentTime) "
+				"VALUES (%s, %s, %s, CURDATE(), CURTIME())")
+		cursor.execute(add, lis)
+		resp = mydb.commit() 
 	except Exception as e:
-		resp = print('Database is upto Date')
+		resp = 'Database is upto Date'
+
 	return resp
 
 
+
+## mail_sender() function is using to send a mail with the current 
+## gold rate with comparison with previous date
 def mail_sender(up_dt, gm_lr, gm_pr, pv_lr, pv_pr, diff, stat):
+
 	sender_email = "esskayrengan@gmail.com"
-	receiver_email = input("\n   Enter The Mail ADDRESS: ")
-#	password = getpass.getpass()
-	password = 'redhat123'
+	print('\n    Sender Email : "esskayrengan@gmail.com"')
+	password = getpass.getpass()
+#	password = 'redhat123'
+	receiver_email = input("\n    Receiver Email : ")
+
 
 	message = MIMEMultipart("alternative")
 	message["Subject"] = "Gold Rate"
@@ -142,13 +172,20 @@ def mail_sender(up_dt, gm_lr, gm_pr, pv_lr, pv_pr, diff, stat):
 # Create secure connection with server and send email
 	context = ssl.create_default_context()
 	with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-	    server.login(sender_email, password)
-	    server.sendmail(
-	        sender_email, receiver_email, message.as_string()
-	    )
+		try:
+			server.login(sender_email, password)
+			server.sendmail(
+	        	sender_email, receiver_email, message.as_string()
+			)
+			resp = print('\n\033[1;32;40m    Mail sent ...!\033[1;0;40m\n')
+		except Exception as e:
+			resp = print('\n\033[1;31;40m    Something went wrong ...!\033[1;0;40m\n')
+	return resp 
 
 
 
+## Main Execution of the script, adding data to DB and
+## Sending the composed mail
 add_data(mydb, cursor, data)
 mail_sender(up_dt, gm_lr, gm_pr, pv_lr, pv_pr, diff, stat)
 
